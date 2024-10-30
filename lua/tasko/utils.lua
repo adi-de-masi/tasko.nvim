27c4c931a4fd021c73b899bade9c7dd8b04570d2
Adi De Masi <adi.demasi@gmail.com>
2024-10-30 06:46:19 +0100

    Write buffer as task to state

---
+++ lua/tasko/utils.lua
@@ -0,0 +1,26 @@
+local utils = {}
+local random = math.random
+function utils.uuid()
+  local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+  return string.gsub(template, '[xy]', function(c)
+    local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+    return string.format('%x', v)
+  end)
+end
+
+function utils.dump(o)
+  if type(o) == 'table' then
+    local s = '{ '
+    for k, v in pairs(o) do
+      if type(k) ~= 'number' then k = '"' .. k .. '"' end
+      s = s .. '[' .. k .. '] = ' .. utils.dump(v) .. ','
+    end
+    return s .. '} '
+  elseif type(o) == 'string' then
+    return ("%q"):format(o)
+  else
+    return tostring(o)
+  end
+end
+
+return utils
---
+++ schema/test.md
@@ -0,0 +1,6 @@
+# This is my test Task
+
+It has a simple body.
+That even may consist of multiple lines.
+
+As many as you wish, actually.
