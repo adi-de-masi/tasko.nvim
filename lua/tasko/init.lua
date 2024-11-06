5e39732b85e2abfbf79886405d14b5c1d5f9eb49
Adi De Masi <adi.demasi@gmail.com>
2024-11-06 22:11:09 +0100

    refactor: move helper function to better place

--- lua/tasko/init.lua
+++ lua/tasko/init.lua
@@ -1,6 +1,22 @@
 local M = {}
 local utils = require '../tasko/utils'
 local Path = require 'plenary.path'
+local task_list_filename = "*tasks*.md"
+local function get_or_create_tasko_directory()
+  -- `~/.config/share/nvim` on unix
+  local data_dir = vim.fn.stdpath("data")
+  if (type(data_dir) == "table") then
+    data_dir = data_dir[1]
+  end
+  local tasko_dir = vim.fs.joinpath(data_dir, "tasko")
+  local tasko_dir_path = Path:new(tasko_dir)
+  if (not tasko_dir_path:exists()) then
+    tasko_dir_path:mkdir()
+  end
+  return tasko_dir
+end
+local base_dir = get_or_create_tasko_directory()
+
 
 M.Task = {}
 function M.Task:new(id, title, body)
@@ -14,7 +30,6 @@ function M.Task:new(id, title, body)
 end
 
 function M.Task:to_task_list_line(task)
-  local base_dir = M.Store:get_or_create_tasko_directory()
   if (task.done ~= nil) then
     return string.format("DONE: [%s](%s/%s.md)", task.title, base_dir, task.id)
   else
@@ -59,18 +74,24 @@ end
 
 M.Store = {}
 
-function M.Store:get_or_create_tasko_directory()
-  -- `~/.config/share/nvim` on unix
-  local data_dir = vim.fn.stdpath("data")
-  if (type(data_dir) == "table") then
-    data_dir = data_dir[1]
+function M.Store:get_task_list_file()
+  -- Task List is already open
+  local buf = utils.get_buf_by_pattern("%*tasks%*.md")
+  if (not buf) then
+    -- Task List is stored in a file
+    local task_list_file = Path:new(
+      vim.fs.joinpath(base_dir, task_list_filename))
+    if (task_list_file:exists()) then
+      -- try to load from file
+      vim.fn.execute(string.format('edit %s', task_list_file:absolute()))
+      buf = vim.api.nvim_get_current_buf()
+    else
+      -- We create a new Task List
+      buf = vim.api.nvim_create_buf(true, false)
+      vim.api.nvim_buf_set_name(buf, task_list_file:absolute())
+      vim.api.nvim_set_option_value("ft", "md", { buf = buf })
+    end
   end
-  local tasko_dir = vim.fs.joinpath(data_dir, "tasko")
-  local tasko_dir_path = Path:new(tasko_dir)
-  if (not tasko_dir_path:exists()) then
-    tasko_dir_path:mkdir()
-  end
-  return tasko_dir
 end
 
 function M.Store:write_task()
@@ -79,7 +100,7 @@ function M.Store:write_task()
     print('nothing written, no task identified')
     return
   end
-  local tasko_dir = self:get_or_create_tasko_directory()
+  local tasko_dir = get_or_create_tasko_directory()
   local target_file = vim.fs.joinpath(tasko_dir, task.id .. ".md")
   local content = table.concat(
     vim.api.nvim_buf_get_lines(
@@ -90,23 +111,28 @@ function M.Store:write_task()
 end
 
 function M.Store:delete(task_id)
-  local base_dir = self:get_or_create_tasko_directory()
   local file_path = vim.fs.joinpath(base_dir, task_id .. ".md")
   return Path:new(file_path):rm()
 end
 
 function M.Store:read(file_name)
-  local tasko_dir = self:get_or_create_tasko_directory()
+  local tasko_dir = get_or_create_tasko_directory()
   local target_file = vim.fs.joinpath(tasko_dir, file_name)
   local file = Path:new(target_file):read()
   return M.Task:from_lines(file)
 end
 
-function M.Store:list_tasks(task_list_filename)
-  local tasko_dir = self:get_or_create_tasko_directory()
+function M.Store:list_tasks(override_task_list_filename)
+  local tasko_dir = get_or_create_tasko_directory()
+  local task_list_file_to_exclude
+  if (override_task_list_filename) then
+    task_list_file_to_exclude = override_task_list_filename
+  else
+    task_list_file_to_exclude = task_list_filename
+  end
   local result = {}
   local i = 1
-  for dir in io.popen("ls -pa " .. tasko_dir .. "  | grep -v / | grep -v \"" .. string.gsub(task_list_filename, "*", "\\*") .. "\""):lines()
+  for dir in io.popen("ls -pa " .. tasko_dir .. "  | grep -v / | grep -v \"" .. string.gsub(task_list_file_to_exclude, "*", "\\*") .. "\""):lines()
   do
     result[i] = dir
     i = i + 1
--- plugin/tasko.lua
+++ plugin/tasko.lua
@@ -1,29 +1,11 @@
 local Store = require('tasko').Store
 local Path = require('plenary.path')
 local base_dir = Store:get_or_create_tasko_directory()
-local task_list_filename = "*tasks*.md"
 local utils = require('tasko').Utils
 
 vim.api.nvim_create_user_command("TaskoList", function()
-  -- Task List is already open
-  local buf = utils.get_buf_by_pattern("%*tasks%*.md")
-  if (not buf) then
-    -- Task List is stored in a file
-    local task_list_file = Path:new(
-      vim.fs.joinpath(base_dir, task_list_filename))
-    if (task_list_file:exists()) then
-      -- try to load from file
-      vim.fn.execute(string.format('edit %s', task_list_file:absolute()))
-      buf = vim.api.nvim_get_current_buf()
-    else
-      -- We create a new Task List
-      buf = vim.api.nvim_create_buf(true, false)
-      vim.api.nvim_buf_set_name(buf, task_list_file:absolute())
-      vim.api.nvim_set_option_value("ft", "md", { buf = buf })
-    end
-  end
-
-  local task_list = require('tasko').Store:list_tasks(task_list_filename)
+  local buf = Store:get_task_list_file()
+  local task_list = require('tasko').Store:list_tasks()
   for _, task_file in ipairs(task_list) do
     local task_lines = Path:new(vim.fs.joinpath(base_dir, task_file)):read()
     local task = require('tasko').Task:from_lines(task_lines)
