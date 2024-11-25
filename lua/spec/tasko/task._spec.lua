619be7538584d0d3b9338cf463b2e1d8e829157f
Adi De Masi <adi.demasi@gmail.com>
2024-11-25 07:25:35 +0100

    wip: change from .md to .tasko
    The format should be as easy as possible, will add custom task file
    later when I'll introduce a path_to_task_file property

--- lua/spec/tasko/task._spec.lua
+++ lua/spec/tasko/task._spec.lua
@@ -1,20 +1,20 @@
-local Task = require('tasko.task')
+local Task = require("tasko.task")
 
-describe('text to task', function()
-  it('Creates a task', function()
-    local task = Task:new(nil, "This is my test Task", "This is the body of my test task")
-    assert(task.title == "This is my test Task", "Title couldn't be parsed.")
-    assert.is_not_nil(task.id)
-  end)
+describe("text to task", function()
+	it("Creates a task", function()
+		local task = Task:new(nil, "This is my test Task", "This is the body of my test task")
+		assert(task.title == "This is my test Task", "Title couldn't be parsed.")
+		assert.is_not_nil(task.id)
+	end)
 
-  it('converts an open markdown file to a task', function()
-    -- loads test file into buffer 1
-    vim.fn.execute('edit lua/spec/tasko/test_task.md', false)
-    local task = Task:from_current_buffer()
-    local expected_title = "This is my test Task"
+	it("converts an open markdown file to a task", function()
+		-- loads test file into buffer 1
+		vim.fn.execute("edit lua/spec/tasko/test_task.md", false)
+		local task = Task:from_current_buffer()
+		local expected_title = "This is my test Task"
 
-    assert(task.title == expected_title, "Title couldn't be parsed. '" .. task.title .. "'")
-    local expected_body = [[
+		assert(task.title == expected_title, "Title couldn't be parsed. '" .. task.title .. "'")
+		local expected_body = [[
 
 
 It has a simple body.
@@ -23,13 +23,19 @@ That even may consist of multiple lines.
 As many as you wish, actually.
 
 ]]
-    assert(task.body == expected_body, "Body couldn't be parsed.")
-    assert(task.id == '12345', "Id couldn't be parsed.")
-  end)
+		assert(task.body == expected_body, "Body couldn't be parsed.")
+		assert(task.id == "12345", "Id couldn't be parsed.")
+	end)
 
-  it('converts a markdown file with the done comment to a task', function()
-    vim.fn.execute('edit lua/spec/tasko/test_done_task.md', false)
-    local task = Task:from_current_buffer()
-    assert(task.done ~= nil, "Task not marked as done")
-  end)
+	it("converts a markdown file with the done comment to a task", function()
+		vim.fn.execute("edit lua/spec/tasko/test_done_task.md", false)
+		local task = Task:from_current_buffer()
+		assert(task.done ~= nil, "Task not marked as done")
+	end)
+
+	it("serializes a task", function()
+		local task = Task:new(nil, "This is my test task")
+		task.priority = 4
+		task.serialize()
+	end)
 end)
